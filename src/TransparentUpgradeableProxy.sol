5fe755cb75d426bb01714bc5399085a2c4aa728e
Matías <mjlescano@protonmail.com>
2023-09-07 15:55:52 +0100

    add TransparentUpgradableProxy from OpenZeppellin

---
+++ src/TransparentUpgradeableProxy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {ProxyAdmin} from "lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy as TransparentUpgradeableProxyBase} from "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract TransparentUpgradeableProxy is TransparentUpgradeableProxyBase {
+  constructor(address _logic, address admin_, bytes memory _data) payable TransparentUpgradeableProxyBase(_logic, admin_, _data) {
+
+  }
+}
