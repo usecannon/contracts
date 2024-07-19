66f87f94578c160f335ddd5b03459ab3f798692f
FuzzB0t <christopherpagnusatt@gmail.com>
2024-07-19 17:18:17 -0600

    Add test for UUPS proxy and add Openzeppelin UUPS proxy package

--- src/UUPS-Proxy/InitialModuleBundle.sol
+++ src/UUPS-Proxy/InitialModuleBundle.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11 <0.9.0;
 
-import "@synthetixio/core-modules/contracts/modules/OwnerModule.sol";
-import "./UpgradeModule.sol";
+import { OwnerModule } from "@synthetixio/core-modules/contracts/modules/OwnerModule.sol";
+import { UpgradeModule } from "./UpgradeModule.sol";
 
 // The below contract is only used during initialization as a kernel for the first release which the system can be upgraded onto.
 // Subsequent upgrades will not need this module bundle
