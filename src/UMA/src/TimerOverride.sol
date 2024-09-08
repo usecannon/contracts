8e992f95e4304cedfd7cf9617a8e802d393fa1aa
Matías <mjlescano@protonmail.com>
2024-09-08 20:52:18 -0300

    fix contract repetition bug

---
+++ src/UMA/src/TimerOverride.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { Timer as TimerBase } from '@uma/core/contracts/common/implementation/Timer.sol';
+
+contract TimerOverride is TimerBase {}
